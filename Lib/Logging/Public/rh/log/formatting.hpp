99cebc8d5708b74080a79724f97b2097ae6b73e9
Arseniy Savchenko <arseny.saw4enko@gmail.com>
2026-03-13 17:22:14 +0300

    Add formatting.hpp

---
+++ Lib/Logging/Public/rh/log/formatting.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <rh/log/message.hpp>
+
+namespace rh::log {
+
+/**
+ * Abstract formatter of rh::log::Message to a String
+ */
+class Formatter {
+public:
+  virtual ~Formatter() = default;
+
+public:
+  // Format the message. "withColors" toggles ANSI color sequences.
+  virtual String Format(Message const& message, bool withColors) = 0;
+};
+
+} // namespace rh::log
